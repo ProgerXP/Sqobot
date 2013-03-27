15712cf8bf7b998c91890a49e2237ef9448bd049
Proger <proger.xp@gmail.com>
2013-03-27 18:46:06 +0400

    Webpatching now supports SQL import.

--- task/webpatch.php
+++ task/webpatch.php
@@ -4,17 +4,34 @@ class TaskWebpatch extends Task {
   public $title = 'Patching';
 
   function do_(array $args = null) {
-    if ($patch = Web::upload('patch')) {
-      empty($args['nodes']) and $this->patch($patch['tmp_name'], $patch['name']);
-      empty($args['self']) and $this->patchNodes($patch['tmp_name'], $patch['name']);
+    $files = Web::upload('files');
+    $sql = Web::upload('sql');
+
+    if ($files or $sql) {
+      if (empty($args['nodes'])) {
+        $files and $this->patchFiles($files['tmp_name'], $files['name']);
+        $files and $sql and print '<hr>';
+        $sql and $this->patchSQLs($sql['tmp_name'], $sql['name']);
+      }
+
+      if (empty($args['self'])) {
+        $this->patchNodes(compact('files', 'sql'));
+      }
+
       return;
     }?>
 
     <form action="." method="post" enctype="multipart/form-data">
+      <input type="hidden" name="task" value="patch">
+
       <p>
-        <input type="hidden" name="task" value="patch">
-        <b>Patch with</b> (ZIP or single file):
-        <input type="file" name="patch">
+        <b>Replace files</b> (ZIP or single file):
+        <input type="file" name="files">
+      </p>
+
+      <p>
+        <b>Execute SQLs</b> (can be ZIP):
+        <input type="file" name="sql">
       </p>
 
   <?php
@@ -31,12 +48,12 @@ class TaskWebpatch extends Task {
     echo '</p></form>';
   }
 
-  function patch($local, $name) {
+  function patchFiles($local, $name) {
     $isZip = S::ext($name) === '.zip';
 
     if (!$isZip) {
       if (copy($local, $name)) {
-        echo HLEx::p('Patched '.HLEx::kbd_q($name).'.', 'ok');
+        echo HLEx::p('Written '.HLEx::kbd_q($name).'.', 'ok');
       } else {
         $local = HLEx::kbd_q($local);
         $name = HLEx::kbd_q(getcwd().DIRECTORY_SEPARATOR.$name);
@@ -53,20 +70,101 @@ class TaskWebpatch extends Task {
         Web::quit(500, "<p>Cannot extract archive to ".HLEx::kbd_q(getcwd()).".</p>");
       }
 
+      $count = static::countZip($zip);
       $zip->close();
-      echo HLEx::p('Patched OK.', 'ok');
+
+      $sfiles = $count['files'] == 1 ? '' : 's';
+      $sdirs = $count['dirs'] == 1 ? '' : 's';
+      echo HLEx::p("Extracted $count[files] file$sfiles and".
+                   " $count[dirs] folder$sdirs.", 'ok');
     }
   }
 
-  function patchNodes($local, $name) {
+    static function countZip($zip) {
+      for ($files = $dirs = $i = 0; $stat = $zip->statIndex($i); ++$i) {
+        substr($stat['name'], -1) === '/' ? ++$dirs : ++$files;
+      }
+
+      return compact('files', 'dirs');
+    }
+
+  function patchSQLs($local, $name) {
+    $isZip = S::ext($name) === '.zip';
+
+    if (!$isZip) {
+      $this->patchSQL(file_get_contents($local));
+    } elseif (!class_exists('ZipArchive')) {
+      Web::quit(500, 'ZipArchive class (php_zip extension) is required.');
+    } else {
+      $zip = new \ZipArchive;
+
+      if (($error = $zip->open($local)) !== true) {
+        Web::quit(500, "Cannot open archive, ZipArchive error #$error.");
+      }
+
+      $files = $affected = 0;
+      ob_start();
+      echo '<ol>';
+
+      for ($i = 0; $name = $zip->getNameIndex($i); ++$i) {
+        if (S::ext($name) === '.sql') {
+          $affected += $this->patchSQL($zip->getFromIndex($i), $name);
+          ++$files;
+        }
+      }
+
+      echo '</ol>';
+
+      $sfiles = $files == 1 ? '' : 's';
+      $saff = $affected == 1 ? '' : 's';
+      $summary = "<b>$affected row$saff affected</b> after running $files file$sfiles:";
+
+      echo HLEx::p($summary, 'summary').
+           ob_get_clean();
+    }
+  }
+
+  function patchSQL($sql, $file = '') {
+    $affected = 0;
+    $errors = array();
+
+    while ($sql) {
+      try {
+        $affected += dbImport($sql);
+        $sql = array();
+      } catch (EDbImport $e) {
+        $errors[] = HLEx::p_q($e->getPrevious()->getMessage(), 'error').
+                    HLEx::pre_q($e->sql, 'gen sql');
+        $affected += $e->affected;
+        $sql = $e->remainingSQLs;
+      }
+    }
+
+    if ($file) {
+      echo HLEx::tag('li', $errors ? 'errors' : ''), HLEx::kbd_q("$file: ");
+    } else {
+      echo '<p>';
+    }
+
+    $s = $affected == 1 ? '' : 's';
+    echo "$affected row$s affected.", join($errors);
+    echo $file ? '</li>' : '</p>';
+
+    return $affected;
+  }
+
+  function patchNodes(array $uploads) {
     foreach (Node::all() as $node) {
       echo HLEx::h3_q('Node '.$node->id());
 
       try {
-        echo $node->call('patch')
-          ->addQuery('self')
-          ->upload('patch', $name, fopen($local, 'rb'))
-          ->fetchData();
+        $req = $node->call('patch')->addQuery('self');
+
+        foreach ($uploads as $var => $upload) {
+          $req->upload($var, $upload['name'], fopen($upload['tmp_name'], 'rb'));
+        }
+
+        echo $req->fetchData();
       } catch (\Exception $e) {
         echo HLEx::p('Error sending request: '.HLEx::kbd_q($msg = exLine($e)).'.');
         error("Cannot contact node {$node->id()} to patch: $msg.");
